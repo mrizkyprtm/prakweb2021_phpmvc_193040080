56f35561ce4fb3ab94c2fc816a3e5004209f0ea6
m_rizkyprtm <m.rizkypratama319@gmail.com>
2021-10-01 23:43:31 +0700

    (video 6) menambahkan asset (framework bootstrap dan gambar)

---
+++ app/core/Constants.php
@@ -0,0 +1,2 @@
+<?php
+define('BASEURL', 'http://localhost/prakweb2021_phpmvc_193040080/public');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>About Me</h1>
-<p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+<div class="container">
+  <h1 class="mt-4">About Me</h1>
+  <img src="<?= BASEURL; ?>/img/profile.jpg" class="rounded-circle shadow" width="200">
+  <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
